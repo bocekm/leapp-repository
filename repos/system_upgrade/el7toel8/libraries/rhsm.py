1213984c1de42d97a02aef1e4859eeef219d51b5
Michal Bocek <mbocek@redhat.com>
2020-03-05 18:48:35 +0100

    Applied review comments

--- repos/system_upgrade/el7toel8/libraries/rhsm.py
+++ repos/system_upgrade/el7toel8/libraries/rhsm.py
@@ -3,22 +3,19 @@ import functools
 import os
 import re
 import time
+from collections import namedtuple
 
+from leapp import reporting
 from leapp.exceptions import StopActorExecutionError
-from leapp.libraries.common.config import get_product_type
 from leapp.libraries.stdlib import CalledProcessError, api
+from leapp.models import RHSMInfo
 
 _RE_REPO_UID = re.compile(r'Repo ID:\s*([^\s]+)')
 _RE_RELEASE = re.compile(r'Release:\s*([^\s]+)')
 _RE_SKU_CONSUMED = re.compile(r'SKU:\s*([^\s]+)')
 _ATTEMPTS = 5
 _RETRY_SLEEP = 5
-
-
-class Repo(object):
-    def __init__(self):
-        self.id = None
-        self.file = None
+_DEFAULT_RHSM_REPOFILE = '/etc/yum.repos.d/redhat.repo'
 
 
 def _rhsm_retry(max_attempts, sleep=None):
@@ -59,7 +56,7 @@ def _rhsm_retry(max_attempts, sleep=None):
 @contextlib.contextmanager
 def _handle_rhsm_exceptions(hint=None):
     """
-    Context manager based function that handles exceptions of `run` for the subscription manager calls.
+    Context manager based function that handles exceptions of `run` for the subscription-manager calls.
     """
     try:
         yield
@@ -83,12 +80,12 @@ def _handle_rhsm_exceptions(hint=None):
 
 
 def skip_rhsm():
-    """ Check whether we should skip RHSM related code """
+    """Check whether we should skip RHSM related code."""
     return os.getenv('LEAPP_DEVEL_SKIP_RHSM', '0') == '1'
 
 
 def with_rhsm(f):
-    """ Decorator to allow skipping RHSM functions by executing a no-op """
+    """Decorator to allow skipping RHSM functions by executing a no-op."""
     if skip_rhsm():
         @functools.wraps(f)
         def _no_op(*args, **kwargs):
@@ -98,71 +95,102 @@ def with_rhsm(f):
 
 
 @with_rhsm
-def get_attached_skus(context, rhsm_info):
+def get_attached_skus(context):
     """
-    Retrieve the list of SKU the system is attached to with the subscription manager.
+    Retrieve the list of the SKUs the system is attached to with the subscription-manager.
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
-    :param rhsm_info: An instance of a RHSMInfo derived model.
-    :type rhsm_info: RHSMInfo derived model
+    :return: SKUs the current system is attached to.
+    :rtype: List(string)
     """
-    if not rhsm_info.attached_skus:
-        with _handle_rhsm_exceptions():
-            result = context.call(['subscription-manager', 'list', '--consumed'], split=False)
-            rhsm_info.attached_skus = _RE_SKU_CONSUMED.findall(result['stdout'])
+    with _handle_rhsm_exceptions():
+        result = context.call(['subscription-manager', 'list', '--consumed'], split=False)
+        return _RE_SKU_CONSUMED.findall(result['stdout'])
 
 
-def get_available_repo_uids(context, rhsm_info):
-    """Deprecated. Repositories have ids, not uids. Use get_available_repo_ids instead."""
-    return get_available_repo_ids(context, rhsm_info)
-
-
-def get_available_repo_ids(context, rhsm_info):
+def get_available_repo_ids(context):
     """
     Retrieve repo ids of all the repositories available through the subscription-manager.
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
-    :param rhsm_info: An instance of a RHSMInfo derived model.
-    :type rhsm_info: RHSMInfo derived model
+    :return: Repositories that are available to the current system through the subscription-manager
+    :rtype: List(string)
     """
-    if not rhsm_info.available_repos:
-        result = context.call(['yum', 'repoinfo'])
-        all_repos = list(get_repos(result['stdout']))
-        rhsm_info.available_repos = [repo.id for repo in all_repos if repo.file == '/etc/yum.repos.d/redhat.repo']
+    result = context.call(['yum', 'repoinfo'])
+    _inhibit_on_duplicate_repos(result['stderr'])
+    all_repos = list(_get_repos(result['stdout']))
+    available_repos = [repo.id for repo in all_repos if repo.file == _DEFAULT_RHSM_REPOFILE]
+    list_separator_fmt = '\n    - '
+    api.current_logger().info('The following repoids are available through RHSM:{0}{1}'.
+                              format(
+                                  list_separator_fmt,
+                                  list_separator_fmt.join(available_repos)
+                              ))
+    return available_repos
 
 
-def get_repos(repos_raw):
+def _inhibit_on_duplicate_repos(repos_raw_stderr):
+    """
+    Inhibit the updgrade if a single repo is defined multiple times.
+
+    When that happens, it not only shows miscofigured system, but then we can't get the details of all the available
+    repos as well.
+    """
+    duplicates = []
+    for duplicate in re.findall(
+            r'Repository ([^\s]+) is listed more than once', repos_raw_stderr, re.DOTALL | re.MULTILINE):
+        duplicates.append(duplicate)
+
+    if not duplicates:
+        return
+    list_separator_fmt = '\n    - '
+    api.current_logger().warn('The following repoids are defined multiple times:{0}{1}'.
+                              format(
+                                  list_separator_fmt,
+                                  list_separator_fmt.join(duplicates)
+                              ))
+
+    reporting.create_report([
+        reporting.Title('A YUM/DNF repository defined multiple times'),
+        reporting.Summary(
+            'The `yum repoinfo` command reports that the following repositories are defined multiple times:{0}{1}'.
+            format(
+                list_separator_fmt,
+                list_separator_fmt.join(duplicates)
+            )
+        ),
+        reporting.Severity(reporting.Severity.MEDIUM),
+        reporting.Tags([reporting.Tags.REPOSITORY]),
+        reporting.Flags([reporting.Flags.INHIBITOR]),
+        reporting.Remediation(hint='Remove the duplicit repository definitions.')
+    ])
+
+
+def _get_repos(repos_raw_stdout):
     """
     Generator providing all the repos available through yum/dnf.
 
     :rtype: Iterator[:py:class:`leapp.libraries.common.rhsm.Repo`]
     """
-    # TODO: Log a warning on the following msg that `yum repoinfo` may produce:
-    #  "Repository rhel-7-server-eus-rpms is listed more than once in the configuration"
-
     # Split all the available repos per one repo
     for repo_raw_params in re.findall(
-            r"Repo-id.*?Repo-filename.*?\n",
-            repos_raw,
-            re.DOTALL | re.MULTILINE):
-        yield parse_repo_params(repo_raw_params)
+            r"Repo-id.*?Repo-filename.*?\n", repos_raw_stdout, re.DOTALL | re.MULTILINE):
+        yield _parse_repo_params(repo_raw_params)
 
 
-def parse_repo_params(repo_raw_params):
+def _parse_repo_params(repo_raw_params):
     """Parse multiline string holding repo parameters to distill the important ones."""
-    repo = Repo()
     try:
-        repo.id = get_repo_param(r"^Repo-id\s+:\s+(.*?)(/.*)?$", repo_raw_params, "Repo-id")
-        repo.file = get_repo_param(r"^Repo-filename:\s+(.*?)$", repo_raw_params, "Repo-filename")
+        repoid = _get_repo_param(r'^Repo-id\s+:\s+([^/]+).*?$', repo_raw_params, 'Repo-id')
+        repofile = _get_repo_param(r'^Repo-filename:\s+(.*?)$', repo_raw_params, 'Repo-filename')
+        return namedtuple('Repository', ['repoid', 'file'])(repoid, repofile)
     except ValueError, err:
         api.current_logger().warn("Failed to parse the '%s' repo parameter of the `yum repoinfo` output", err.args[0])
 
-    return repo
 
-
-def get_repo_param(pattern, repo_raw_params, param):
+def _get_repo_param(pattern, repo_raw_params, param):
     """Parse a string with all the repo params to get the value of a single repo param."""
     sub_attr = re.search(pattern,
                          repo_raw_params,
@@ -170,35 +198,29 @@ def get_repo_param(pattern, repo_raw_params, param):
     if sub_attr:
         return sub_attr.group(1)
     else:
-        raise ValueError(param)
-
-
-def get_enabled_repo_uids(context, rhsm_info):
-    """Deprecated. Repositories have ids, not uids. Use get_enabled_repo_ids instead."""
-    return get_enabled_repo_ids(context, rhsm_info)
+        raise ValueError(param, repo_raw_params)
 
 
 @with_rhsm
-def get_enabled_repo_ids(context, rhsm_info):
+def get_enabled_repo_ids(context):
     """
-    Retrieve names of all the repositories enabled through the subscription-manager.
+    Retrieve repo ids of all the repositories enabled through the subscription-manager.
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
-    :param rhsm_info: An instance of a RHSMInfo derived model.
-    :type rhsm_info: RHSMInfo derived model
+    :return: Repositories that are enabled on the current system through the subscription-manager.
+    :rtype: List(string)
     """
-    if not rhsm_info.enabled_repos:
-        with _handle_rhsm_exceptions():
-            result = context.call(['subscription-manager', 'repos', '--list-enabled'], split=False)
-            rhsm_info.enabled_repos = _RE_REPO_UID.findall(result['stdout'])
+    with _handle_rhsm_exceptions():
+        result = context.call(['subscription-manager', 'repos', '--list-enabled'], split=False)
+        return _RE_REPO_UID.findall(result['stdout'])
 
 
 @with_rhsm
 @_rhsm_retry(max_attempts=_ATTEMPTS, sleep=_RETRY_SLEEP)
 def unset_release(context):
     """
-    Unset the configured release from the subscription manager.
+    Unset the configured release from the subscription-manager.
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
@@ -215,7 +237,7 @@ def set_release(context, release):
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
-    :param release: Release to set the subscription manager to.
+    :param release: Release to set the subscription-manager to.
     :type release: str
     """
     with _handle_rhsm_exceptions():
@@ -223,20 +245,19 @@ def set_release(context, release):
 
 
 @with_rhsm
-def get_release(context, rhsm_info):
+def get_release(context):
     """
-    Retrieves the release the subscription manager has been pinned to, if applicable.
+    Retrieves the release the subscription-manager has been pinned to, if applicable.
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
-    :param rhsm_info: An instance of a RHSMInfo derived model.
-    :type rhsm_info: RHSMInfo derived model
+    :return: Release the subscription-manager is set to.
+    :rtype: string
     """
-    if rhsm_info.release is None:
-        with _handle_rhsm_exceptions():
-            result = context.call(['subscription-manager', 'release'], split=False)
-            result = _RE_RELEASE.findall(result['stdout'])
-            rhsm_info.release = result[0] if result else ''
+    with _handle_rhsm_exceptions():
+        result = context.call(['subscription-manager', 'release'], split=False)
+        result = _RE_RELEASE.findall(result['stdout'])
+        return result[0] if result else ''
 
 
 @with_rhsm
@@ -253,24 +274,23 @@ def refresh(context):
 
 
 @with_rhsm
-def get_existing_product_certificates(context, rhsm_info):
+def get_existing_product_certificates(context):
     """
     Retrieves information about existing product certificates on the system.
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
-    :param rhsm_info: An instance of a RHSMInfo derived model.
-    :type rhsm_info: RHSMInfo derived model
+    :return: Paths to product certificates that are currently installed on the system.
+    :rtype: List(string)
     """
-    if not rhsm_info.existing_product_certificates:
-        for path in ('/etc/pki/product', '/etc/pki/product-default'):
-            if not os.path.isdir(context.full_path(path)):
-                continue
-            certs = [os.path.join(path, f) for f in os.listdir(context.full_path(path))
-                     if os.path.isfile(os.path.join(context.full_path(path), f))]
-            if not certs:
-                continue
-            rhsm_info.existing_product_certificates.extend(certs)
+    for path in ('/etc/pki/product', '/etc/pki/product-default'):
+        if not os.path.isdir(context.full_path(path)):
+            continue
+        certs = [os.path.join(path, f) for f in os.listdir(context.full_path(path))
+                 if os.path.isfile(os.path.join(context.full_path(path), f))]
+        if not certs:
+            continue
+        return certs
 
 
 @contextlib.contextmanager
@@ -283,9 +303,9 @@ def switch_certificate(context, rhsm_info, cert_path):
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
-    :param rhsm_info: An instance of a RHSMInfo derived model.
-    :type rhsm_info: RHSMInfo derived model
-    :param cert_path: Path to the product certificate to switch to.
+    :param rhsm_info: An instance of the RHSMInfo model
+    :type rhsm_info: RHSMInfo model
+    :param cert_path: Path to the product certificate to switch to
     :type cert_path: string
     """
     # Back up product certificates
@@ -310,17 +330,19 @@ def switch_certificate(context, rhsm_info, cert_path):
 
 
 @with_rhsm
-def scan_rhsm_info(context, rhsm_info):
+def scan_rhsm_info(context):
     """
-    Gather all RHSM information
+    Gather all the RHSM information.
 
     :param context: An instance of a mounting.IsolatedActions class
     :type context: mounting.IsolatedActions class
-    :param rhsm_info: An instance of a RHSMInfo derived model.
-    :type rhsm_info: RHSMInfo derived model
+    :return: An instance of an RHSMInfo model.
+    :rtype: RHSMInfo model
     """
-    get_attached_skus(context, rhsm_info)
-    get_available_repo_ids(context, rhsm_info)
-    get_enabled_repo_ids(context, rhsm_info)
-    get_release(context, rhsm_info)
-    get_existing_product_certificates(context, rhsm_info)
+    info = RHSMInfo()
+    info.attached_skus = get_attached_skus(context)
+    info.available_repos = get_available_repo_ids(context)
+    info.enabled_repos = get_enabled_repo_ids(context)
+    info.release = get_release(context)
+    info.existing_product_certificates.extend(get_existing_product_certificates(context))
+    return info
