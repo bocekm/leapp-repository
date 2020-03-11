bb12e438606c55cdb3a8e6b088827e9b22a2ecd9
Petr Stodulka <pstodulk@redhat.com>
2020-03-11 10:33:31 +0100

    reportsettargetrelease: refactoring + fix tests

--- repos/system_upgrade/el7toel8/actors/reportsettargetrelease/actor.py
+++ repos/system_upgrade/el7toel8/actors/reportsettargetrelease/actor.py
@@ -1,8 +1,6 @@
-from leapp import reporting
 from leapp.actors import Actor
-from leapp.libraries.stdlib import api
+from leapp.libraries.actor import library
 from leapp.models import Report
-from leapp.reporting import create_report
 from leapp.tags import IPUWorkflowTag, TargetTransactionChecksPhaseTag
 
 
@@ -17,16 +15,4 @@ class ReportSetTargetRelease(Actor):
     tags = (IPUWorkflowTag, TargetTransactionChecksPhaseTag)
 
     def process(self):
-        target_version = api.current_actor().configuration.version.target
-        create_report([
-            reporting.Title(
-                'The subscription-manager release is going to be set to {release}'.format(release=target_version)),
-            reporting.Summary(
-                'After the upgrade has completed the release of the subscription-manager will be set to {release}.'
-                ' This will ensure that you will receive and keep the version you choose to upgrade to.'.
-                format(release=target_version)
-            ),
-            reporting.Severity(reporting.Severity.LOW),
-            reporting.Tags([reporting.Tags.UPGRADE_PROCESS]),
-            reporting.RelatedResource('package', 'subscription-manager')
-        ])
+        library.process()
