cff96d446e8ff6bf7e762be47ab7a8b4e1011899
Michal Bocek <mbocek@redhat.com>
2020-03-12 11:04:07 +0100

    Update messaging about setting the submgr release

--- repos/system_upgrade/el7toel8/actors/reportsettargetrelease/libraries/library.py
+++ repos/system_upgrade/el7toel8/actors/reportsettargetrelease/libraries/library.py
@@ -5,18 +5,18 @@ from leapp.libraries.stdlib import api
 def process():
     # TODO: skip if users are not using rhsm at all (RHELLEAPP-201)
     target_version = api.current_actor().configuration.version.target
-    # The non-static title is against our best-practicess, but in this case
-    # it make sense to print the specific info. Anyway, feel free to change
-    # it to static string.
     reporting.create_report([
         reporting.Title(
-            'The subscription-manager release is going to be set to {release}'.format(release=target_version)),
+            'The subscription-manager release is going to be set after the upgrade'),
         reporting.Summary(
             'After the upgrade has completed the release of the subscription-manager will be set to {release}.'
             ' This will ensure that you will receive and keep the version you choose to upgrade to.'
             .format(release=target_version)
         ),
         reporting.Severity(reporting.Severity.LOW),
+        reporting.Remediation(
+            hint='If you wish to receive updates for the latest released version of RHEL 8, run `subscription-manager'
+                 ' release --unset` after the upgrade.'),
         reporting.Tags([reporting.Tags.UPGRADE_PROCESS]),
         reporting.RelatedResource('package', 'subscription-manager')
     ])
